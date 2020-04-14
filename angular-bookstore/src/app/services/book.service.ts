aa7d1f896d6d6c20e350410784136713b420c86a
Ratan <Ratan@Ratan-PC>
2020-04-14 15:59:58 +0530

    Completed the feature 3 search by category.

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -7,10 +7,11 @@ import { Book } from '../common/book';
   providedIn: 'root'
 })
 export class BookService {
-  private baseUrl="http://localhost:8081/api/v1/books";
+  private baseUrl="http://localhost:8082/api/v1/books";
   constructor(private httpClient:HttpClient) { }
-  getBooks():Observable<Book[]>{
-    return this.httpClient.get<GetResponseBooks>(this.baseUrl).pipe(
+  getBooks(theCategoryId:number):Observable<Book[]>{
+    const searchUrl=`${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponseBooks>(searchUrl).pipe(
       map(response=>response._embedded.books)
     ); 
   }
--- onlinebookstore/src/main/java/in/risithratan/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/risithratan/onlinebookstore/repository/BookRepository.java
@@ -1,10 +1,15 @@
 package in.risithratan.onlinebookstore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.risithratan.onlinebookstore.entity.Book;
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+	@RestResource(path="categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id,Pageable pageable);
 }
